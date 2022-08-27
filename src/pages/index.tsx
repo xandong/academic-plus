ab055b47f4a0f802d77631166bf852b152a55457
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-08-26 23:38:06 -0300

    Initial commmit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function PageIndex() {
+  return (
+    <div>
+      <Head>
+        <title>Academic Plus</title>
+      </Head>
+      <h1>Academic Plus</h1>;
+    </div>
+  );
+}
