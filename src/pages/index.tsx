5d09b949503e255aa7119fe36110122307b2f6dc
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-08-31 17:51:59 -0300

    refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,18 @@
 import Head from "next/head";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 export default function PageIndex() {
   return (
-    <div>
+    <>
       <Head>
         <title>Academic Plus</title>
       </Head>
       <Header />
-      <h1>Academic Plus</h1>
-    </div>
+      <main className="h-[200vh] bg-zinc-700 text-zinc-50">
+        <h1>Academic Plus</h1>
+      </main>
+      <Footer />
+    </>
   );
 }
