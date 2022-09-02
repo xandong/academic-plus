e0d86042bcd2eb6c26468967a69466e96b034f5a
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-09-02 14:44:18 -0300

    style(Main & Footer): modify style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import { ArrowUp } from "phosphor-react";
 
 export default function Footer() {
   return (
-    <footer className="flex justify-around items-center w-full p-8">
+    <footer className="flex justify-around items-baseline w-full p-8">
       <Link href="/">
         <a>
           <Logo />
@@ -15,8 +15,11 @@ export default function Footer() {
         <a>
           <ArrowUp
             size={32}
-            className="p-1 bg-zinc-700 rounded-md text-zinc-50 shadow-md
-        hover:shadow-lg hover:bg-zinc-800 transition-colors duration-200"
+            weight="bold"
+            className="p-1 bg-zinc-800 text-zinc-50 border-zinc-800 border-2
+            hover:p-px hover:shadow-lg hover:bg-zinc-50 hover:text-zinc-800 
+            shadow-md rounded-md
+            animate-bounce transition-all duration-500"
           />
         </a>
       </Link>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -7,9 +7,10 @@ interface IMainProps {
 export default function Main({ children }: IMainProps) {
   return (
     <main
-      className="min-h-[90vh] flex-1 py-10 md:px-40 sm:px-8 bg-white
-    flex justify-center flex-col items-center gap-10 
-    relative"
+      className="min-h-[90vh] flex-1
+      flex flex-col justify-center items-center gap-10 
+      py-10 md:px-40 sm:px-8 bg-white
+      relative"
     >
       {children}
     </main>
