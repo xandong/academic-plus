af29b57ae7abef7e3c93c56ab593fddc8dd21972
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-09-02 02:16:05 -0300

    style(login): add gradient in background

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -22,65 +22,84 @@ export default function Login() {
       <Head>
         <title>Entre no A+ 📘</title>
       </Head>
-      <Header />
-      <main className="py-10 md:px-40 sm:px-8 flex justify-center sm:justify-end sm:flex-row flex-col items-center gap-10 bg-white relative">
-        {isMinScreen ? (
-          <div className="">
-            <Image
-              className="bg-cover"
-              src={digital}
-              height="400px"
-              layout="intrinsic"
-              alt="Uma digital e uma pessoa"
-            />
-          </div>
-        ) : (
-          ""
-        )}
+      <div className="min-w-screen min-h-screen flex flex-col">
+        <Header />
+        <main
+          className="flex-1 md:px-40 flex 
+          flex-col
+          justify-center items-center
+          
 
-        <form
-          onSubmit={handleSubmit}
-          autoComplete="on"
-          id="form"
-          className="max-w-full flex flex-col gap-6 h-fit sm:p-12 py-12 px-8 flex-1 sm:flex-initial  bg-white sm:border-b-4 sm:border-r-4 border-primary-500 sm:rounded-2xl sm:shadow-xl"
+          bg-gradient-to-r from-blue-500
+          to-cyan-500 
+          relative"
         >
-          <h1 className="text-5xl font-semibold text-primary-500 text-center">
-            Login
-          </h1>
+          {isMinScreen ? (
+            <div className="">
+              <Image
+                className="bg-cover"
+                src={digital}
+                height="400px"
+                layout="intrinsic"
+                alt="Uma digital e uma pessoa"
+              />
+            </div>
+          ) : (
+            ""
+          )}
 
-          <Input
-            key="email"
-            type="email"
-            label="Email"
-            value={email}
-            setValue={setEmail}
-          />
-          <Input
-            key="password"
-            type="password"
-            label="Senha"
-            value={password}
-            setValue={setPassword}
-          />
+          <form
+            onSubmit={handleSubmit}
+            autoComplete="on"
+            id="form"
+            className="flex-1 flex flex-col gap-6 h-fit sm:p-12 py-12 px-8 w-full 
+            bg-white
+            sm:w-[420px]
+            sm:flex-none
+            sm:my-10  
+            sm:border-b-8 sm:border-r-4 sm:rounded-2xl sm:shadow-xl"
+          >
+            <h1 className="text-5xl font-semibold text-primary-500 text-center">
+              Login
+            </h1>
 
-          <Button text="Entrar" type="submit" />
+            <Input
+              key="email"
+              type="email"
+              label="Email"
+              value={email}
+              setValue={setEmail}
+            />
+            <Input
+              key="password"
+              type="password"
+              label="Senha"
+              value={password}
+              setValue={setPassword}
+            />
+            <fieldset className="flex gap-1 justify-end items-center">
+              <input type="checkbox" name="" id="checkbox" />
+              <label htmlFor="checkbox">Mantenha conectado</label>
+            </fieldset>
+            <Button text="Entrar" type="submit" />
 
-          <small className="text-right text-sm font-medium text-primary-500">
-            Esqueceu a senha?
-            <Link href="/">
-              <a
-                className="hover:text-primary-600 focus:outline-none
+            <small className="text-right text-sm font-medium text-primary-500">
+              Esqueceu a senha?
+              <Link href="/">
+                <a
+                  className="hover:text-primary-600 focus:outline-none
               focus:text-primary-600  transition    
               focus:border-b-2
               focus:border-b-primary-600"
-              >
-                {" "}
-                Clique aqui
-              </a>
-            </Link>
-          </small>
-        </form>
-      </main>
+                >
+                  {" "}
+                  Clique aqui
+                </a>
+              </Link>
+            </small>
+          </form>
+        </main>
+      </div>
     </>
   );
 }
