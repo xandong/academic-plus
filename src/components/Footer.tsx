3a9ff32f4935f895cce9cccf0961f224d89cb01a
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-09-02 02:11:31 -0300

    feat: add button to Home

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,23 @@
 import Link from "next/link";
 import Logo from "./Logo";
 
+import { ArrowUp } from "phosphor-react";
+
 export default function Footer() {
   return (
-    <footer className="flex w-full p-8">
-      <Link href="#">
+    <footer className="flex justify-around items-center w-full p-8">
+      <Link href="/">
         <a>
           <Logo />
+        </a>
+      </Link>
+      <Link href="#">
+        <a>
+          <ArrowUp
+            size={32}
+            className="p-1 bg-zinc-700 rounded-md text-zinc-50 shadow-md
+        hover:shadow-lg hover:bg-zinc-800 transition-colors duration-200"
+          />
         </a>
       </Link>
     </footer>
