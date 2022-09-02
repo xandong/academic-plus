4c293ac96c9397a757058ad9f9b9e323519bc714
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-09-02 02:17:04 -0300

    create Page Profile

---
+++ src/pages/profile.tsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Main from "../components/Main";
+
+export default function Profile() {
+  return (
+    <>
+      <Head>
+        <title>A+: Perfil 💁</title>
+      </Head>
+      <Header />
+      <Main>
+        <div className="container">
+          <p>
+            The
+            <abbr title="Web Hypertext Application Technology Working Group">
+              WHATWG
+            </abbr>
+            started working on HTML5 in 2004.
+          </p>
+        </div>
+        <div className="container"></div>
+      </Main>
+      <Footer />
+    </>
+  );
+}
