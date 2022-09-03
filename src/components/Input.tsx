752d351e6e9cd7817a7e58c94de7650fe2aa3f53
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-09-03 14:24:19 -0300

    add icons & focus dynamic

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,10 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, ReactNode, useState } from "react";
 
 interface IInputProps {
   id?: string;
   type: string;
   label: string;
+  Icon?: ReactNode;
   value: string;
   setValue: Dispatch<SetStateAction<string>>;
   messageError?: string;
@@ -15,12 +16,33 @@ export default function Input({
   id = type,
   label,
   value,
+  Icon,
   setValue,
   messageError,
   required = true,
 }: IInputProps) {
+  // const isValid = () => {
+  //   if (type === "password") {
+  //     if (value.length < 8 && value !== "") {
+  //       messageError = "A senha contém mais de 7 dígitos";
+  //       return "cancel-hover";
+  //     } else {
+  //       return "success-hover";
+  //     }
+  //   } else {
+  //     return "primary-500";
+  //   }
+  // };
+
+  const isPasswordValid =
+    type === "password" && value !== ""
+      ? (messageError = "A senha contém mais de 7 dígitos") && value.length < 8
+        ? "cancel-hover"
+        : "success-hover"
+      : "primary-500";
+
   return (
-    <fieldset className="flex flex-col group">
+    <fieldset className="flex flex-col group relative">
       <label htmlFor={id} className="text-zinc-900 font-medium">
         {label}
       </label>
@@ -31,12 +53,14 @@ export default function Input({
         type={type}
         value={value}
         onChange={(e) => setValue(e.target.value)}
-        className="p-2 rounded bg-zinc-50 border-b-2 border-zinc-200 max-w-full
-        hover:border-primary-500 focus:outline-none
-        focus:border-primary-500 transition-colors duration-200"
+        className={`p-2 ${
+          Icon ? "pl-8" : ""
+        } rounded bg-zinc-50 border-b-2 border-zinc-200 max-w-full
+        hover:border-primary-500 transition-colors duration-200
+        focus:border-${isPasswordValid} focus:outline-none`}
         required={required}
       />
-
+      <div className="top-9 left-2 absolute">{Icon}</div>
       <span className="text-cancel-hover text-sm font-medium">
         {messageError}
       </span>
