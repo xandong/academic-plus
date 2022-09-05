9b71d725731693d013d1b0f116263d23e2b632d4
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-09-05 19:50:33 -0300

    style(CardItem): add border

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -19,7 +19,7 @@ export default function CardItem({
     <li className="mb-6">
       <Link href={rote}>
         <a
-          className="w-[240px] h-[240px] bg-zinc-white dark:bg-zinc-700 shadow-md hover:shadow-xl rounded-lg flex flex-col gap-6 justify-center items-center transition-all
+          className="w-[240px] h-[240px] bg-zinc-white dark:bg-zinc-700 border dark:border-zinc-600 shadow-md hover:shadow-xl rounded-lg flex flex-col gap-6 justify-center items-center transition-all
         hover:scale-105 focus:scale-105"
         >
           <Image src={img} alt={alt} width="183px" height="96px" />
